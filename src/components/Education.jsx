4ed60116c53780b0f31d6f1a0a35bf8e2417efd2
alexanderjblack <86830695+alexanderjblack@users.noreply.github.com>
2024-03-18 01:08:15 +1000

    Project Section + Header Fonts
    Started to add gameloaded to the projects section and fixed an issue with the wrong font being user for headers

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,11 +3,11 @@ import gradImage from '../assets/graduation.jpg'
 const Education = () => {
   return (
     <div name='education' className='w-full'>
-        <div className='max-w-[1000px] mx-auto px-8 flex flex-col-reverse md:flex-row-reverse justify-center items-center h-full gap-2 md:gap-12 py-8 md:py-24'>
-          <div className='text-primary font-[800] max-w-[500px] align-top'>
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col-reverse md:flex-row-reverse justify-center items-center h-full gap-2 md:gap-12 py-8 md:py-32'>
+          <div className='text-primary font-[700] max-w-[450px] align-top'>
             <h1 className='text-l text-center md:text-xl pt-2 md:text-left text-secondary'>EDUCATION</h1>
             <h1 className='text-xl text-center md:text-2xl py-2 md:text-left'>Bachelor of Computer Science, University of Queensland</h1>
-            <p className='text-center  pb-8 font-[500] md:text-left text-l md:text-[18px]'> Graduated December, 2023 with a major in Cybersecurity. Selected to undertake a semester exchange to the University of California, Los Angeles.</p>
+            <p className='text-center  pb-8 font-[500] md:text-left text-l md:text-[18px]'> Graduated in December, 2023 with a major in Cybersecurity. Selected to undertake an exchange semester at the University of California, Los Angeles.</p>
 
           </div>
           <div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,9 +3,20 @@ import React from 'react'
 const Projects = () => {
   return (
     <div name='projects' className='w-full bg-[#f9f9f9]'>
-        <div className='py-8 md:py-16 px-8 max-w-[1000px] mx-auto justify-center flex flex-col'>
-            <h1 className='text-l  font-[800] text-center md:text-xl py-2  text-secondary'>PROJECTS</h1>
-            <p className='text-center font-[500]  text-[18px]'> Formal education is great - but I'm most proud of my personal projects.</p>
+        <div className='py-8 md:py-32 px-8 max-w-[1000px] mx-auto justify-center flex flex-col '>
+            <div className="pb-2">
+                <h1 className='text-l  font-[800] text-center md:text-xl py-1  text-secondary'>PROJECTS</h1>
+                <p className='text-center font-[500]  text-[18px]'> Formal education is great and all - but I'm most proud of my personal projects.</p>
+            </div>
+            <div className='bg-primary text-white rounded-xl py-2 px-4 mt-4 mb-2 md:py-3 flex flex-row justify-between'>
+                <h2 className=' font-[600]'>GameLoaded</h2>
+                <h2 className=' font-[600]'>2019 - Present</h2>
+            </div>
+
+            <div className='bg-white text-primary rounded-xl py-8 px-4 flex flex-col justify-between'>
+                <p className=' font-[600]'>GameLoaded is a video game development studio founded by my brother, Lucas. The company has 8 full-time employees and has amassed over 20 million hours of gameplay from players of our games.</p>
+
+            </div>
         </div>
     </div>
   )
