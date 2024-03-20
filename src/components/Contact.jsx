d00c9350a7bb2f2b9123adbf983ee1fa9071f162
alexanderjblack <86830695+alexanderjblack@users.noreply.github.com>
2024-03-20 21:39:07 +1000

    Contact section complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,18 +3,18 @@ import { MdOutlineEmail } from "react-icons/md";
 
 const Contact = () => {
   return (
-    <div name='skills' className='w-full bg-white'>
+    <div name='skills' className='w-full bg-[#f9f9f9]'>
         <div className='py-12 md:py-24 px-8 max-w-[1000px] mx-auto justify-center flex flex-col'>
           <div className="pb-2">
               <h1 className='text-l  font-[800] text-center md:text-xl py-1  text-secondary'>CONTACT</h1>
-              <h1 className='text-xl text-center md:text-2xl  pb-4 text-primary font-[700] '>Reach out to me!</h1>
-              <p className='text-center font-[600] text-xl'>Beyond anything, I'm a problem solver.</p>
-              <div className='flex flex-row justify-center items-center gap-4'>
-                    <div className='text-4xl'>
+              <h1 className='text-xl text-center md:text-2xl  pb-2 text-primary font-[700] '>Reach out to me!</h1>
+              <p className='text-center font-[600] text-xl'>If you liked this site I made, then imagine what I could do for you.</p>
+              <div className='flex flex-row justify-center items-center gap-4 py-6'>
+                    <div className='text-4xl text-secondary shadow-[0_0_10px_0_rgba(0,0,0,0.19)] p-2 rounded-full'>
                         <MdOutlineEmail/>
                     </div>
                     <div>
-                        <h3>Email</h3>
+                        <h3 className='font-[700]'>Email</h3>
                         <p>alexanderblack957@gmail.com</p>
                     </div>
               </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
 
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col-reverse md:flex-row justify-center items-center h-full gap-2 md:gap-12'>
           <div className='text-primary font-[700] max-w-[500px] align-top'>
-            <h1 className='text-4xl text-center md:text-5xl py-6 md:text-left'>Hey there! I'm Alexander 👋</h1>
+            <h1 className='text-4xl text-center md:text-5xl py-6 md:text-left transition-opacity ease-in duration-700'>Hey there! I'm Alexander 👋</h1>
             <p className='text-center font-[500] md:text-left text-[18px]'> I'm looking for a software engineering position - and this website will detail why you should hire me. Brisbane, Australia.</p>
             <div className='py-4 text-3xl text-secondary flex justify-center md:justify-start'> <CiLinkedin /></div>
           </div>
